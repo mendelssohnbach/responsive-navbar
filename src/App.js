e7f3d0a621edcdd714652549c73bb28f42a45c41
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-13 07:25:17 +0900

    :tada: First commit.

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavMenu from './components/NavMenu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Project from './pages/Project';
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <NavMenu />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/project">
+            <Project />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
---
+++ src/pages/Project.jsx
@@ -0,0 +1,7 @@
+export default function Project() {
+  return (
+    <div>
+      <h1>Project page</h1>
+    </div>
+  );
+}
